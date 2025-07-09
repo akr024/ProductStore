9205bf3da5dfd23fa5a3104371d5cec97623ee36
Abhimanyu Kumar <abhimanyuku024@gmail.com>
2025-07-09 14:49:46 -0700

    locate port in env for security

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import {connectDB} from './config/db.js'
 import productRoutes from './routes/product.route.js'
+import dotenv from 'dotenv'
+
+dotenv.config();
 
 const app = express();
 
@@ -8,7 +11,7 @@ app.use(express.json()); //middleware which parses json
 
 app.use('/api/products', productRoutes);
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
     connectDB();
     console.log("Server started at http://localhost:3000")
 })
