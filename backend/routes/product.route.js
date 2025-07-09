481bf803a652eba00a115d8f5e75ea4cbac5cf64
Abhimanyu Kumar <abhimanyuku024@gmail.com>
2025-07-09 14:47:09 -0700

    basic backend developed

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -3,11 +3,8 @@ import * as productCode from '../controllers/product.controller.js'
 const router = express.Router();
 
 router.get('/', productCode.getProducts)
-
 router.post('/', productCode.postProduct);
-
 router.patch('/:id', productCode.patchProduct)
-
 router.delete('/:id', productCode.deleteProduct)
 
 export default router;
