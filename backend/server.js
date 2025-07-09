dd9809b5fd676244c3cdd1b5b0879ac7d52bbd83
Abhimanyu Kumar <abhimanyuku024@gmail.com>
2025-07-09 00:41:22 -0700

    set up models, mongodb and nodemon config

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,15 @@
 import express from 'express';
+import {connectDB} from './config/db.js'
 
 const app = express();
 
 app.get('/', (req,res)=>{
-    res.send("server is ready!")
+    res.send("server is ready!");
 })
 
+
 app.listen(3000, () => {
-    console.log("Server started at port http://localhost:3000")
+    connectDB();
+    console.log("Server started at http://localhost:3000")
 })
 
